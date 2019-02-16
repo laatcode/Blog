51614b270eb7b0af060f06ab40ae2bafae5ef26e
Luis Angel Avila Torres <luisangelavilatorres@gmail.com>
2019-02-16 17:11:39 -0500

    Implementación de edición de posts

--- app/controllers/admin/PostController.php
+++ app/controllers/admin/PostController.php
@@ -18,6 +18,14 @@ class PostController extends BaseController {
     ]);
   }
 
+  public function validatePost() {
+    $validator = new Validator();
+    $validator->add('title', 'required');
+    $validator->add('content', 'required');
+
+    return $validator->validate($_POST);
+  }
+
   public function getCreate() {
     $user = User::find($_SESSION['userId']);
     return $this->render('admin/insert-post.twig', [
@@ -30,11 +38,7 @@ class PostController extends BaseController {
     $result = false;
     $user = User::find($_SESSION['userId']);
 
-    $validator = new Validator();
-    $validator->add('title', 'required');
-    $validator->add('content', 'required');
-
-    if ($validator->validate($_POST)) {
+    if ($this.validatePost()) {
       $blogPost = new BlogPost([
         'title' => $_POST['title'],
         'content' => $_POST['content'],
@@ -57,6 +61,45 @@ class PostController extends BaseController {
       'result' => $result,
       'errors' => $errors,
       'loggedUser' => $user
+    ]);
+  }
+
+  public function getEdit($id) {
+    $blogPost = BlogPost::find($id);
+    $user = User::find($_SESSION['userId']);
+    return $this->render('admin/edit-post.twig', [
+      'loggedUser' => $user,
+      'blogPost' => $blogPost
+    ]);
+  }
+
+  public function postEdit($id) {
+    $errors = [];
+    $result = false;
+    $user = User::find($_SESSION['userId']);
+
+    if ($this->validatePost()) {
+      $blogPost = BlogPost::find($id);
+      $blogPost->title = $_POST['title'];
+      $blogPost->content = $_POST['content'];
+
+      if ($_FILES['img']['name']) {
+        $img_src = "images/post_images/" . uniqid() . $_FILES['img']['name'];
+        move_uploaded_file($_FILES['img']['tmp_name'], $img_src);
+        $blogPost->img_src = $img_src;
+      }
+
+      $blogPost->save();
+      $result = true;
+    }else {
+      $errors = $validator->getMessages();
+    }
+
+    return $this->render('admin/edit-post.twig', [
+      'loggedUser' => $user,
+      'errors' => $errors,
+      'result' => $result,
+      'blogPost' => $blogPost
     ]);
   }
 
